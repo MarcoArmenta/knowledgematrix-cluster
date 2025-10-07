5c89f7acbe48630b02267a5a35d37b5ddc0f2c3c
Marco Armenta <drmarcoarmenta@gmail.com>
2025-10-06 23:52:51 -0400

    allow for more input shapes

--- knowledgematrix/matrix_computer.py
+++ knowledgematrix/matrix_computer.py
@@ -25,8 +25,16 @@ class KnowledgeMatrixComputer:
         self.batch_size = batch_size
         self.layers = model.layers
         self.device = device
-        self.in_c, self.in_h, self.in_w = model.input_shape
-        self.input_size = self.in_c*self.in_h*self.in_w
+        if len(model.input_shape) == 3:
+            self.in_c, self.in_h, self.in_w = model.input_shape
+            self.input_size = self.in_c*self.in_h*self.in_w
+        elif len(model.input_shape) == 1:
+            self.input_size = 1
+            self.in_c = 1
+            self.in_h = model.input_shape[0]
+            self.in_w = 1
+        else:
+            raise ValueError(f"Non suported shape {model.shape}")
 
         # Saves the output of the NN on the current sample in the forward method
         self.current_output: Union[NN, None] = None
